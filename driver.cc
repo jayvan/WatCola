307e01cfa35e5c0ee404be529283afe311d60117
Justin Vanderheide <justin@justinvanderheide.com>
2013-11-20 20:57:12 -0500

    driver

--- driver.cc
+++ driver.cc
@@ -1,11 +1,39 @@
 #include "MPRNG.h"
 #include "printer.h"
+#include "config.h"
 #include <iostream>
 
 using namespace std;
 
 MPRNG prng;
 
+void usage(char* argv[]) {
+  cout << "usage: " <<  argv[0] << " [ conﬁg-ﬁle [ random-seed > 0 ] ]" << endl;
+  exit(1);
+}
+
 void uMain::main() {
-  cout << "Hello World" << endl;
+  // Default values
+  prng.seed(getpid());
+  char* configPath = (char*)"soda.config";
+
+  // Overload defaults from command line arguments
+  switch (argc) {
+    case 3:
+      if (atoi(argv[3]) <= 0) {
+        usage(argv);
+      }
+      prng.seed(atoi(argv[3]));
+    case 2:
+      configPath = argv[2];
+  }
+
+  // Process configuration file
+  ConfigParms params;
+  processConfigFile(configPath, params);
+
+
+  // Create in order: printer, bank, parent, WATCard ofﬁce, name server, vending machines, bottling plant, and students.
+  Printer p(params.numStudents, params.numVendingMachines, params.numCouriers);
+
 }
